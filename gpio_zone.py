d682e62b568582975695546482520b4e66bcd086
Will Holderness <will@unikey.com>
2016-12-03 17:33:22 -0500

    Adding base functionality

--- gpio_zone.py
+++ gpio_zone.py
@@ -1,5 +1,36 @@
+import sys
+
 try:
     import RPi.GPIO as GPIO
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+
+seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
+
+
+def convert_to_seconds(s):
+        return int(s[:-1]) * seconds_per_unit[s[-1]]
+
+if len(sys.argv) < 2 or int(sys.argv[1]) > len(zone_list):
+    print("bad args")
+    quit() 
+
+GPIO.setmode(GPIO.BOARD)
+
+zone_list = [11,12,13]
+
+GPIO.setup(zone_list, GPIO.OUT)
+zone = sys.argv[1] - 1
+runtime = convert_to_seconds(sys.argv[2])
+
+print("Zone " + zone + " for " + runtime + " seconds.")
+
+GPIO.output(zone,GPIO.HIGH)
+
+time.sleep(runtime)
+
+GPIO.output(zone,GPIO.LOW)
+
+GPIO.cleanup()
 
